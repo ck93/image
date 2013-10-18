76743fa18b45eda7abb441370073ea7dbac563a0
ck93 <chenkaiwelcome@gmail.com>
2013-10-18 22:22:47 +0800

    完成了图片的读取和存储

---
+++ Image/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Image.Properties.Resources.LENA;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.BackColor = System.Drawing.Color.Transparent;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonX1.Location = new System.Drawing.Point(593, 161);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(6);
+            this.buttonX1.Size = new System.Drawing.Size(82, 33);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 1;
+            this.buttonX1.Text = "打开图像";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "图像文件|*.bmp;*.jpg";
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.BackColor = System.Drawing.Color.Transparent;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonX2.Location = new System.Drawing.Point(593, 230);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(6);
+            this.buttonX2.Size = new System.Drawing.Size(82, 33);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX2.TabIndex = 2;
+            this.buttonX2.Text = "保存图像";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "bmp";
+            this.saveFileDialog1.Filter = "BMP文件|*.bmp|JPEG文件|*.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 562);
+            this.Controls.Add(this.buttonX2);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Image/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Image
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap image;
+        int width = 512;
+        int height = 512;
+        int[,] R;
+        int[,] G;
+        int[,] B;
+        void GetRGB()
+        {
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = image.LockBits(rect, ImageLockMode.ReadWrite, image.PixelFormat);
+            IntPtr iPtr = bmpData.Scan0;
+            int iBytes = width * height * 3;
+            byte[] PixelValues = new byte[iBytes];
+            System.Runtime.InteropServices.Marshal.Copy(iPtr, PixelValues, 0, iBytes);
+            image.UnlockBits(bmpData);
+            R = new int[height, width];
+            G = new int[height, width];
+            B = new int[height, width];
+            int iPoint = 0;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    // 注意，Windows 中三基色的排列顺序是 BGR 而不是 RGB！
+                    B[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    G[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    R[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                }
+            }
+        }
+        Bitmap FromRGB()
+        {
+            Bitmap Result = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = Result.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            IntPtr iPtr = bmpData.Scan0;
+            int iStride = bmpData.Stride;
+            int iBytes = width * height * 3;
+            byte[] PixelValues = new byte[iBytes];
+            int iPoint = 0;
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    PixelValues[iPoint] = Convert.ToByte(B[i, j]);
+                    PixelValues[iPoint + 1] = Convert.ToByte(G[i, j]);
+                    PixelValues[iPoint + 2] = Convert.ToByte(R[i, j]);
+                    iPoint += 3;
+                }
+            System.Runtime.InteropServices.Marshal.Copy(PixelValues, 0, iPtr, iBytes);
+            Result.UnlockBits(bmpData);
+            return Result;
+        }
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string pic = openFileDialog1.FileName;
+                image = new Bitmap(pic);
+                if (image == null)
+                {
+                    MessageBox.Show("图片加载失败！", "错误");
+                    return;
+                }
+                width = image.Width;
+                height = image.Height;
+                GetRGB();
+                pictureBox1.Image = FromRGB();
+                
+            }
+        }
+
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+                MessageBox.Show("图片保存成功","成功");
+            }
+        }
+    }
+}
