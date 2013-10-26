113c935f493c54920737769317c4298f6b46bc9d
ck93 <chenkaiwelcome@gmail.com>
2013-10-26 22:17:39 +0800

    完成旋转扭曲

--- Image/Form1.Designer.cs
+++ Image/Form1.Designer.cs
@@ -33,6 +33,15 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.slider1 = new DevComponents.DotNetBar.Controls.Slider();
+            this.switchButton1 = new DevComponents.DotNetBar.Controls.SwitchButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonX3 = new DevComponents.DotNetBar.ButtonX();
+            this.comboBoxEx1 = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.comboItem0 = new DevComponents.Editors.ComboItem();
+            this.comboItem1 = new DevComponents.Editors.ComboItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.slider2 = new DevComponents.DotNetBar.Controls.Slider();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,6 +55,7 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // buttonX1
             // 
@@ -53,13 +63,13 @@
             this.buttonX1.BackColor = System.Drawing.Color.Transparent;
             this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
             this.buttonX1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.buttonX1.Location = new System.Drawing.Point(593, 161);
+            this.buttonX1.Location = new System.Drawing.Point(596, 30);
             this.buttonX1.Name = "buttonX1";
             this.buttonX1.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(6);
             this.buttonX1.Size = new System.Drawing.Size(82, 33);
             this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.buttonX1.TabIndex = 1;
-            this.buttonX1.Text = "打开图像";
+            this.buttonX1.Text = "载入图像";
             this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
             // 
             // openFileDialog1
@@ -72,12 +82,12 @@
             this.buttonX2.BackColor = System.Drawing.Color.Transparent;
             this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
             this.buttonX2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.buttonX2.Location = new System.Drawing.Point(593, 230);
+            this.buttonX2.Location = new System.Drawing.Point(596, 147);
             this.buttonX2.Name = "buttonX2";
             this.buttonX2.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(6);
             this.buttonX2.Size = new System.Drawing.Size(82, 33);
             this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.buttonX2.TabIndex = 2;
+            this.buttonX2.TabIndex = 3;
             this.buttonX2.Text = "保存图像";
             this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
             // 
@@ -86,19 +96,146 @@
             this.saveFileDialog1.DefaultExt = "bmp";
             this.saveFileDialog1.Filter = "BMP文件|*.bmp|JPEG文件|*.jpg";
             // 
+            // slider1
+            // 
+            // 
+            // 
+            // 
+            this.slider1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.slider1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.slider1.LabelWidth = 40;
+            this.slider1.Location = new System.Drawing.Point(547, 319);
+            this.slider1.Maximum = 40;
+            this.slider1.Minimum = 1;
+            this.slider1.Name = "slider1";
+            this.slider1.Size = new System.Drawing.Size(160, 30);
+            this.slider1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.slider1.TabIndex = 5;
+            this.slider1.Text = "强度";
+            this.slider1.Value = 20;
+            this.slider1.ValueChanged += new System.EventHandler(this.slider1_ValueChanged);
+            // 
+            // switchButton1
+            // 
+            // 
+            // 
+            // 
+            this.switchButton1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.switchButton1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.switchButton1.Location = new System.Drawing.Point(625, 207);
+            this.switchButton1.Name = "switchButton1";
+            this.switchButton1.OffText = "顺时针";
+            this.switchButton1.OnText = "逆时针";
+            this.switchButton1.Size = new System.Drawing.Size(86, 30);
+            this.switchButton1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.switchButton1.TabIndex = 7;
+            this.switchButton1.ValueChanged += new System.EventHandler(this.switchButton1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(543, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "旋转方向";
+            // 
+            // buttonX3
+            // 
+            this.buttonX3.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX3.BackColor = System.Drawing.Color.Transparent;
+            this.buttonX3.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonX3.Location = new System.Drawing.Point(596, 88);
+            this.buttonX3.Name = "buttonX3";
+            this.buttonX3.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(6);
+            this.buttonX3.Size = new System.Drawing.Size(82, 33);
+            this.buttonX3.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX3.TabIndex = 2;
+            this.buttonX3.Text = "还原";
+            this.buttonX3.Click += new System.EventHandler(this.buttonX3_Click);
+            // 
+            // comboBoxEx1
+            // 
+            this.comboBoxEx1.DisplayMember = "Text";
+            this.comboBoxEx1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBoxEx1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxEx1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxEx1.FormattingEnabled = true;
+            this.comboBoxEx1.ItemHeight = 17;
+            this.comboBoxEx1.Items.AddRange(new object[] {
+            this.comboItem0,
+            this.comboItem1});
+            this.comboBoxEx1.Location = new System.Drawing.Point(625, 261);
+            this.comboBoxEx1.Name = "comboBoxEx1";
+            this.comboBoxEx1.Size = new System.Drawing.Size(86, 23);
+            this.comboBoxEx1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.comboBoxEx1.TabIndex = 4;
+            // 
+            // comboItem0
+            // 
+            this.comboItem0.Text = "最近邻插值";
+            // 
+            // comboItem1
+            // 
+            this.comboItem1.Text = "双线性插值";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(545, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "插值算法";
+            // 
+            // slider2
+            // 
+            // 
+            // 
+            // 
+            this.slider2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.slider2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.slider2.LabelWidth = 40;
+            this.slider2.Location = new System.Drawing.Point(547, 370);
+            this.slider2.Maximum = 10;
+            this.slider2.Minimum = 1;
+            this.slider2.Name = "slider2";
+            this.slider2.Size = new System.Drawing.Size(160, 30);
+            this.slider2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.slider2.TabIndex = 6;
+            this.slider2.Text = "范围";
+            this.slider2.Value = 5;
+            this.slider2.ValueChanged += new System.EventHandler(this.slider2_ValueChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 562);
+            this.ClientSize = new System.Drawing.Size(734, 550);
+            this.Controls.Add(this.slider2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxEx1);
+            this.Controls.Add(this.buttonX3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.switchButton1);
+            this.Controls.Add(this.slider1);
             this.Controls.Add(this.buttonX2);
             this.Controls.Add(this.buttonX1);
             this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +246,15 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private DevComponents.DotNetBar.ButtonX buttonX2;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private DevComponents.DotNetBar.Controls.Slider slider1;
+        private DevComponents.DotNetBar.Controls.SwitchButton switchButton1;
+        private System.Windows.Forms.Label label1;
+        private DevComponents.DotNetBar.ButtonX buttonX3;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx comboBoxEx1;
+        private DevComponents.Editors.ComboItem comboItem0;
+        private DevComponents.Editors.ComboItem comboItem1;
+        private System.Windows.Forms.Label label2;
+        private DevComponents.DotNetBar.Controls.Slider slider2;
     }
 }
 
--- Image/Form1.cs
+++ Image/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
+using System.Threading;
 
 namespace Image
 {
@@ -15,14 +16,21 @@ namespace Image
         public Form1()
         {
             InitializeComponent();
+            image = Properties.Resources.LENA;
+            GetRGB();
+            comboBoxEx1.SelectedIndex = 0;
         }
         Bitmap image;
         int width = 512;
         int height = 512;
+        const double pi = 3.1416;
+        
         int[,] R;
         int[,] G;
         int[,] B;
-        void GetRGB()
+        int click_x;
+        int click_y;
+        void GetRGB()//读取图片，数据存储到三个矩阵
         {
             Rectangle rect = new Rectangle(0, 0, width, height);
             BitmapData bmpData = image.LockBits(rect, ImageLockMode.ReadWrite, image.PixelFormat);
@@ -39,14 +47,15 @@ namespace Image
             {
                 for (int j = 0; j < width; j++)
                 {
-                    // 注意，Windows 中三基色的排列顺序是 BGR 而不是 RGB！
+                    //Windows 中三基色的排列顺序是 BGR 而不是 RGB
                     B[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
                     G[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
                     R[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
                 }
             }
         }
-        Bitmap FromRGB()
+
+        Bitmap FromRGB(int[,] R, int[,] G, int[,] B)//由矩阵信息还原图片
         {
             Bitmap Result = new Bitmap(width, height, PixelFormat.Format24bppRgb);
             Rectangle rect = new Rectangle(0, 0, width, height);
@@ -68,6 +77,39 @@ namespace Image
             Result.UnlockBits(bmpData);
             return Result;
         }
+        int max(int m, int n)
+        {
+            return (m > n ? m : n);
+        }
+        double[] rotate(int center_x, int center_y, int x, int y, int strengh, int scale, int limit)//变换后图像中某点对应原来图像中的位置
+        {           
+            double[] site = new double[2];
+            double r = Math.Sqrt((center_x - x) * (center_x - x) + (center_y - y) * (center_y - y));
+            double angle = Math.Atan2((double)(y - center_y), (double)(x - center_x));           
+            double delta = max(limit - (int)Math.Sqrt(11 - scale) * (int)r, 0) / (double)limit;
+            if (switchButton1.Value == true)
+                angle -= strengh * Math.Pow(delta, 3) / 10;               
+            else
+                angle += strengh * Math.Pow(delta, 3) / 10;               
+            site[0] = center_x + r * Math.Cos(angle);
+            site[1] = center_y + r * Math.Sin(angle);
+            return site;
+        }
+        int near(int[,] color, double[] site)
+        {
+            int x = (int)(site[0] * 2 - (int)site[0]);
+            int y = (int)(site[1] * 2 - (int)site[1]);
+            return color[x, y];
+            //return color[(int)site[0],(int)site[1]];
+        }
+        int Biliner(int[,] color, double[] site)
+        {
+            double x0 = site[0];
+            double y0 = site[1];
+            int x = (int)x0;
+            int y = (int)y0;
+            return (int)((x0 - x) * (y0 - y) * color[x,y] + (x0 - x) * (y + 1 - y0) * color[x,y+1]+ (x + 1 - x0) * (y0 - y) * color[x+1,y] + (x + 1 - x0) * (y + 1 - y0) * color[x+1,y+1]);
+        }
         private void buttonX1_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
@@ -82,7 +124,7 @@ namespace Image
                 width = image.Width;
                 height = image.Height;
                 GetRGB();
-                pictureBox1.Image = FromRGB();
+                pictureBox1.Image = FromRGB(R, G, B);
                 
             }
         }
@@ -94,6 +136,83 @@ namespace Image
                 pictureBox1.Image.Save(saveFileDialog1.FileName);
                 MessageBox.Show("图片保存成功","成功");
             }
+        }
+
+        void twist(int x,int y)
+        {
+            int strengh = slider1.Value;
+            int scale = slider2.Value;
+            int limit = x;
+            if (width - x < limit)
+                limit = width - x;
+            if (y < limit)
+                limit = y;
+            if (height - y < limit)
+                limit = height - y;
+            int[,] newR = new int[height, width];
+            int[,] newG = new int[height, width];
+            int[,] newB = new int[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    double[] site = rotate(x, y, i, j, strengh, scale, limit);
+                    if (site[0] > -0.5 && site[0] < width - 1.5 && site[1] > -0.5 && site[1] < height - 1.5)
+                    {                        
+                        switch (comboBoxEx1.SelectedIndex)
+                        {
+                            case 0:
+                                newR[i, j] = near(R, site);
+                                newG[i, j] = near(G, site);
+                                newB[i, j] = near(B, site);
+                                break;
+                            case 1:
+                                newR[i, j] = Biliner(R, site);
+                                newG[i, j] = Biliner(G, site);
+                                newB[i, j] = Biliner(B, site);
+                                break;
+                        }
+                        
+                    }
+                    else
+                    {
+                        newR[i, j] = R[i, j];
+                        newG[i, j] = G[i, j];
+                        newB[i, j] = B[i, j];
+                    }
+
+                }
+            }
+            Bitmap newpic = FromRGB(newR, newG, newB);
+            pictureBox1.Image = newpic;
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int y = e.X * width / 500;
+            int x = e.Y * height / 500;
+            click_x = x;
+            click_y = y;
+        }
+
+        private void slider1_ValueChanged(object sender, EventArgs e)
+        {
+            twist(click_x,click_y);
+        }
+
+        private void switchButton1_ValueChanged(object sender, EventArgs e)
+        {
+            twist(click_x, click_y);
+        }
+
+        private void buttonX3_Click(object sender, EventArgs e)
+        {
+            Bitmap originpic = FromRGB(R, G, B);
+            pictureBox1.Image = originpic;
+        }
+
+        private void slider2_ValueChanged(object sender, EventArgs e)
+        {
+            twist(click_x, click_y);
         }
     }
 }
